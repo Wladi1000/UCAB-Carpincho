9c5d56eeb2259bbafe9ef35b8df21721847f0e2b
Wladi1000 <wladimirss14@gmail.com>
2023-02-15 16:57:10 -0400

    Primer dia de front - login

---
+++ src/components/login.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="login">
+        <div class="login__form">
+            <form action="">
+                <div class="login__form__welcome">
+                    <h1>¡Bienvenido a Practicas Profesionales!</h1>
+                    <h2>Inicia sesión para continuar</h2>
+                </div>
+                <div class="login__form__mail">
+                    <h2>Correo</h2>
+                    <input type="text" placeholder="example.nro@est.ucab.edu.ve">
+                </div>
+                <div class="login__form__password">
+                    <h2>Contraseña</h2>
+                    <input type="password" placeholder="...........................">
+                </div>
+                <button>Iniciar Sesion</button>
+            </form>
+        </div>
+    </div>
+    
+</template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="navbar">
+        <img src="../assets/ucab - isotipo.png" alt="">
+    </div>
+</template>
