bdbbe42a783afc0a3e83d062d2245b86095f68ed
Wladi1000 <wladimirss14@gmail.com>
2023-02-23 16:01:06 -0400

    Estructura de Planillas
    Se han reestructurado las planillas correspondientes a la solicitud, revision y comite de escuela.

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,10 +5,10 @@
             <li class="navbar__list__element"><router-link to="/">Login</router-link></li>
             <li class="navbar__list__element"><router-link to="/upload-propouse">Upload<br>propuose</router-link></li>
             <li class="navbar__list__element"><router-link to="/requests">Requests</router-link></li>
-            <li class="navbar__list__element"><router-link to="/reviewers">Reviewers</router-link></li>
-            <li class="navbar__list__element"><router-link to="/committee">Committee</router-link></li>
-            <li class="navbar__list__element"><router-link to="/council">Council</router-link></li>
-            <li class="navbar__list__element"><router-link to="/assignment">Assignment</router-link></li>
+            <li class="navbar__list__element"><router-link to="/reviewers">Planilla Solicitudes</router-link></li>
+            <li class="navbar__list__element"><router-link to="/committee">Planilla de revision</router-link></li>
+            <li class="navbar__list__element"><router-link to="/council">Planilla revision Profesor</router-link></li>
+            <li class="navbar__list__element"><router-link to="/assignment">Planilla del comite</router-link></li>
         </ul>
     </div>
 </template>
---
+++ src/components/planillaRespuestaRevisor.vue
@@ -0,0 +1,45 @@
+<script setup>
+</script>
+<template>
+    <div class="planilla">
+        <div>
+            <h2>Tema propuesto:</h2>
+            <labe>este es un tema propuesto</labe>
+        </div>
+        <div>
+            <h2>Organizacion</h2>
+            <label for="">Esto es una organizacion</label>
+        </div>
+        <div>
+            <h2>Criterios de evaliacion: </h2>
+            <form @submit.prevent="addCriterio">
+                <input v-model="newCriterio">
+                <button>Add Criterio</button>    
+            </form>
+            <ul>
+                <li v-for="criterio in criterios" :key="criterio.id">
+                    <p>{{ criterio.nombre }}</p>
+                    <input type="radio">
+                    <input type="radio">
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<style>
+    .planilla{
+        padding: 15px;
+        width: 500px;
+        background-color: #D9D9D9;
+    }
+    .planilla h2{
+        margin: 10px 0;
+    }
+    .planilla div{
+        display:flex;
+        flex-direction: column;
+    }
+    label, button{
+        margin: 15px 0;
+    }
+</style>
