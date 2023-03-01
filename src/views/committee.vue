38f21eea92c7674f0e85e9a2d6bf5953d722eb6b
Wladi1000 <wladimirss14@gmail.com>
2023-03-01 17:03:29 -0400

    Seteo de la vista de comité
    Se preparó los contenedores de los elementos en la vista de comité de trabajo de grado.

--- src/views/committee.vue
+++ src/views/committee.vue
@@ -5,9 +5,33 @@
 </script>
 <template>
   <div class="committee">
-    <h1>Soy Comite de Escuela</h1>
+    
+    <div class="committee__container">
 
-    <PlanillaRevisor />
+      <div class="committe__container__display">
+        <div class="committe__container__display__controllers">
+          <div class="committe__container__display__list">
+            <!-- Aqui va el registro para las propuestas de trabajo de grado -->
+          </div>
+        </div>
+      </div>
+      <div class="committe__container__preview">
+        <form action="" class="committe__container__preview__form">
+
+        </form>
+        <div class="create-state">
+          <div class="progressbar">
+            <div class="progressbar--content">
+              
+            </div>
+          </div>
+          <div class="create-carousel">
+            <!-- aqui van los formularios necesarios para el proceso de crear una asignacion de revisor a la propuesta -->
+          </div>
+        </div>
+      </div>
+
+    </div>
 
   </div>
 </template>
