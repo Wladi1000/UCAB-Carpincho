1db5752ad18cb63c5d2484a447af7cba4775eaa0
Wladi1000 <wladimirss14@gmail.com>
2023-03-06 10:21:25 -0400

    Fin de modulo ¨Solicitud de trabajo de grado¨

--- restapi-postgres-v2/src/routes/ProfesionalesExternos.routes.js
+++ restapi-postgres-v2/src/routes/ProfesionalesExternos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { crearProfesionalesExternos,obtenerProfesionalesExternos,actualizarProfesionalesExternos,eliminarProfesionalesExternos,buscarProfesionalesExternos } from '../controllers/ProfesionalesExternos.controller.js'
+import { crearProfesionalesExternos,obtenerProfesionalesExternos,actualizarProfesionalesExternos,eliminarProfesionalesExternos,buscarProfesionalesExternos,buscarProfesionalesExternosByCedula } from '../controllers/ProfesionalesExternos.controller.js'
 const router = Router();
 router.get('/ProfesionalesExternos',obtenerProfesionalesExternos);
 router.post('/ProfesionalesExternos',crearProfesionalesExternos);
 router.put('/ProfesionalesExternos/:id',actualizarProfesionalesExternos);
 router.delete('/ProfesionalesExternos/:id',eliminarProfesionalesExternos);
 router.get('/ProfesionalesExternos/:id',buscarProfesionalesExternos);
+router.get('/BuscarProfesional/:id',buscarProfesionalesExternosByCedula);
 
 
 export default router;
